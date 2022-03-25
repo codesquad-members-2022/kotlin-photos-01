eab1717b2a8f213b15f7e9b7087e5b83b67b6bad
ese111 <feung94@gmail.com>
2022-03-25 16:10:51 +0900

    refactor: 피드백 수정과 deprecated된 함수 사용 제거

--- app/src/main/java/com/example/photoalbum/ShowImageViewModel.kt
+++ app/src/main/java/com/example/photoalbum/ShowImageViewModel.kt
@@ -1,9 +1,6 @@
 package com.example.photoalbum
 
 import android.content.Context
-import android.content.Intent
-import android.media.MediaScannerConnection
-import android.net.Uri
 import android.os.Environment
 import android.util.Log
 import androidx.lifecycle.LiveData
@@ -11,15 +8,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.Glide
+import com.example.photoalbum.data.AlbumRectangle
 import com.example.photoalbum.data.ImageData
 import com.example.photoalbum.model.ShowImageRepositoryRemoteImpl
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.json.JSONArray
 import java.io.*
-import android.media.MediaScannerConnection.MediaScannerConnectionClient as MediaScannerConnectionClient
 
 class ShowImageViewModel: ViewModel() {
 
@@ -32,6 +26,20 @@ class ShowImageViewModel: ViewModel() {
     }
 
     private val showImageRepository = ShowImageRepositoryRemoteImpl()
+
+    var gridList = MutableLiveData<List<AlbumRectangle>>()
+
+    fun initList(){
+        gridList.value = createAlbumRectangle()
+    }
+
+    private fun createAlbumRectangle(): List<AlbumRectangle> {
+        val albumRectangleList = mutableListOf<AlbumRectangle>()
+        for (i in 0 until 40) {
+            albumRectangleList.add(AlbumRectangle(i))
+        }
+        return albumRectangleList
+    }
 
     fun getImage(){
         viewModelScope.launch {
@@ -76,34 +84,29 @@ class ShowImageViewModel: ViewModel() {
     fun selectImageLoad(context: Context): List<File> {
         val fileList = mutableListOf<File>()
         val manager = Glide.with(context)
-        Log.d("SaveTest", "next selectImageLoading")
         imageList.forEach {
             if(it.selected) {
                     val file = manager.downloadOnly().load(it.image).submit().get()
-                    Log.d("SaveTest", "ImageDownLoading")
                     fileList.add(file)
-                    Log.d("SaveTest", "ImageDown ${file.path}")
             }
         }
         return fileList
     }
 
-    fun saveImage(context: Context, file: File) {
-        val downloadPath = "/storage/emulated/0/Download"
-        var localFile = File(downloadPath)
+    fun saveImage(file: File) : Boolean {
+        val downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        var localFile = File(downloadPath.path)
         if (!localFile.exists()) {
             localFile.mkdirs()
         }
-        Log.d("SaveTest", "localFile ${localFile.path}")
-        val filePath = downloadPath + "/" + System.currentTimeMillis() + ".jpeg"
+
+        val filePath = downloadPath.path + "/" + System.currentTimeMillis() + ".jpeg"
         localFile = File(filePath)
-        Log.d("SaveTest", "filePath ${filePath}")
         try {
             val fileInputStream = FileInputStream(file)
             val bufferedInputStream = BufferedInputStream(fileInputStream)
             val byteArrayOutputStream = ByteArrayOutputStream()
 
-            val imageByte = ByteArray(1024)
             var current = -1
 
             while (true) {
@@ -111,16 +114,12 @@ class ShowImageViewModel: ViewModel() {
                 if(current == -1) {
                     break
                 }
-//                Log.d("SaveTest", "${current}")
                 byteArrayOutputStream.write(current)
-//                Log.d("SaveTest", "write1")
             }
-            Log.d("SaveTest", "write2")
             val fileOutputStream = FileOutputStream(localFile)
 
             fileOutputStream.write(byteArrayOutputStream.toByteArray())
 
-            Log.d("SaveTest", "write3")
             fileOutputStream.flush()
             fileOutputStream.close()
             fileInputStream.close()
@@ -130,18 +129,18 @@ class ShowImageViewModel: ViewModel() {
 
 //            val mediaScannerConnection = MediaScannerConnection(context, mediaScannerConnectionClient)
 //            mediaScannerConnection.connect()
-            scanMedia(context, localFile)
-
+//            scanMedia(context, localFile)
+            return true
         } catch (e : Exception) {
             e.printStackTrace()
+            return false
         }
-
     }
 
-    fun scanMedia(context: Context, file: File) {
-        val intent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
-        intent.data = Uri.fromFile(file)
-        Log.d("SaveTest", "intent ${intent.data}")
-        context.sendBroadcast(intent)
-    }
+//    private fun scanMedia(context: Context, file: File) {
+//        val intent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+//        intent.data = Uri.fromFile(file)
+//        Log.d("SaveTest", "intent ${intent.data}")
+//        context.sendBroadcast(intent)
+//    }
 }
